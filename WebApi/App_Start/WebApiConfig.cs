f5754684aa3de060e24c87ec121775e1547807b9
Chuck Lu <chuck.lu@qq.com>
2019-01-17 19:38:56 +0800

    use Autofac.WebApi2 to do the dependency injection for resolver

--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web.Http;
 
 namespace WebApi
@@ -10,6 +13,7 @@ namespace WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            InitAutofac();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -20,5 +24,27 @@ namespace WebApi
                 defaults: new { id = RouteParameter.Optional }
             );
         }
+
+        private static void InitAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            // OPTIONAL: Register the Autofac model binder provider.
+            builder.RegisterWebApiModelBinderProvider();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
     }
 }
