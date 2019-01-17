ff86c626030397c976fe386164754fbec7bf4a20
Chuck Lu <chuck.lu@qq.com>
2019-01-17 18:09:46 +0800

    return void action results

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public void Post()
+        {
+        }
+    }
+}
